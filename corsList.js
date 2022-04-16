eb4fde19aafcd30c4f5313b1da5161b661d8f109
swaroopc236 <swaroopc236@gmail.com>
2022-04-17 00:21:13 +0530

    server setup

---
+++ corsList.js
@@ -0,0 +1,25 @@
+const corsAllowList = [
+    'http://localhost:8100', 
+    'http://localhost:8101',  
+    'http://localhost:8102', 
+    'http://localhost:8103',
+    'http://localhost:8104',
+    'http://localhost:8106',
+    'http://localhost:8107',
+    'http://localhost:8108',
+    'http://localhost:8109',
+    'http://localhost:8110',
+    'http://localhost:8111',
+    'http://localhost:8112',
+    'http://localhost:8113',
+    'http://localhost:8114',
+    'http://localhost:8115',
+    'http://localhost:8116',
+    'http://localhost:8117',
+    'http://localhost:8118',
+    'http://localhost:8119',
+    'http://localhost:8120',
+    'http://localhost:8200'
+]
+
+module.exports = corsAllowList;
---
+++ index.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+const allowlist = require('./corsList');
+// console.log(allowlist);
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+// var allowlist = corsAllowList;
+// console.log(allowList);
+var corsOptionsDelegate = function (req, callback) {
+  var corsOptions;
+  if (allowlist.indexOf(req.header('Origin')) !== -1) {
+    corsOptions = { origin: true, credentials: true } // reflect (enable) the requested origin in the CORS response
+  } else {
+    corsOptions = { origin: false } // disable CORS for this request
+  }
+  callback(null, corsOptions); // callback expects two parameters: error and options
+}
+
+app.use(cors(corsOptionsDelegate));
+app.use(express.json());
+
+app.get('/', (req, res) => {
+	res.status(200).json({
+		msg: 'Base path',
+	});
+});
+
+const userRoutes = require('./users/routes');
+
+app.use('/users', userRoutes);
+
+var server = app.listen(port, () => {
+	console.log('Server is running on port: ' + port);
+});
+
+require('./db');
